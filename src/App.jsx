36a16c93ffd63f24f5773008047f6cc1d5f0404f
itzSumma <itzSumma11@gmail.com>
2026-03-12 22:36:10 +0600

    onclick function made here

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
 import "./App.css";
 
 function App() {
+
+function handleClick(){
+  alert (" I am clicked")
+}
+const  handleClick2=()=>{alert("You have press here")}
+
+const handleAdd =(num)=>{
+  const newNum = num + 5;
+  alert(newNum);
+}
   return (
     <>
-      <h1>hello</h1>
-      <button>click me</button>
+      <h1>Hello</h1>
+
+
+      {/* {<button onclick="handleClick()">Click Me</button>} */}
+
+      {/* function reference */}
+      <button onClick={handleClick}>Press Me</button>
+
+      {/* <button onClick={ function handleClick(){ alert("You have clicked")}}>Press Here</button> */}
+       
+       {/* arrow/Inline function   */}
+      <button onClick={() => alert("Pressed here!")}>
+        Press Here
+      </button>  
+{/* Arrow function variable */}
+      <button onClick={handleClick2}>Click me Too</button>
+      <button onClick={()=> alert(" Click 3")}>Click Me 3</button>  
+      <button onClick={()=>handleAdd(5)}>Click Add</button>
     </>
   );
 }
